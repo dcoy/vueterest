0febcb36b8aa1100f8303b2e0170c73179782286
David Coy <dcoy@gitlab.com>
2019-03-23 21:04:30 -0500

    Models and graphql mutations created

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "typeDefs.gql");
+const typeDefs = fs.readFileSync(filePath, "utf-8");
+const resolvers = require("./resolvers");
+
 require("dotenv").config({ path: "variables.env" });
+const User = require("./models/User");
+const Post = require("./models/Post");
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log("DB connected"))
   .catch(err => console.error(err));
 
-const typeDefs = gql`
-  type Todo {
-    task: String
-    completed: Boolean
-  }
-
-  type Query {
-    getTodos: [Todo]
-  }
-`;
-
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  resolvers,
+  context: {
+    User,
+    Post
+  }
 });
 
 server.listen().then(({ url }) => {
